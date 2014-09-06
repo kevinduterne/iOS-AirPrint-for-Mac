33457e86bba186fd711af8ad7707e08b2596c80f
Kevin Duterne <icewax_98@yahoo.com>
2014-09-07 00:31:17 +0200

    Fix HP Photosmart + space # fix needed for bash

--- Airprint.sh
+++ Airprint.sh
@@ -7,10 +7,11 @@
 ComputerName=`scutil --get ComputerName`
 
 ## Get all bonjour printers for printers belonging to $ComputerName and print to file "/tmp/printerlist.txt"
-dns-sd -B _ipp._tcp local | colrm 1 73 | grep -v 'Instance Name' | sort | uniq | grep ${ComputerName} > /tmp/printerlist.txt & sleep 1 & killall dns-sd
+dns-sd -B _ipp._tcp local | colrm 1 73 > /tmp/printerlist.txt & sleep 1 & killall dns-sd
+cat /tmp/printerlist.txt | grep -v 'Instance Name' | sort | uniq | grep ${ComputerName} > /tmp/printerlist.txt
 
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
+#SAVEIFS=$IFS
+#IFS=$(echo -en "\n\b")
 
 ## List printers
 printers=`cat /tmp/printerlist.txt`
@@ -19,24 +20,25 @@ printers=`cat /tmp/printerlist.txt`
 ## Adds the urf format and "transparent=T binary=T" settings which I believe are needed
 
 for i in $printers; do
-dns-sd -L "$i" _ipp._tcp local | grep 'product=' | tr -d \'\\\\\(\) | sed "s@pwg-raster@urf URF=W8,SRGB24,CP255,RS300@g" |  sed "s/$/ transparent=T binary=T/" | sed 's/note.*priority/note= priority/g' > /tmp/"$i" & sleep 0 &
-killall dns-sd
+txt=`echo "$i" | sed 's/\ /+/g'`
+echo $i;
+echo $txt;
+dns-sd -L "$i" _ipp._tcp local > /tmp/"$i"  & sleep 1 & killall dns-sd
+Options=`cat /tmp/"$i" | grep 'product=' | tr -d \'\\\\\(\) | sed "s@pwg-raster@urf URF=W8,CP1,RS300-600,DM3,SRGB24@g" | sed "s/$/ transparent=T binary=T/" | sed 's/note.*priority/note= priority/g' | uniq`
+echo $Options;
+dns-sd -R "$i""2" _ipp._tcp,_universal . 631 $Options & sleep 1 &
 done
-
-
-
-
 
 for i in $printers; do
+IFS=$(echo -en "\n\b")
 Options=`cat /tmp/"$i"`
-
-dns-sd -R "$i" _ipp._tcp,_universal . 631 $Options & sleep 0 &
-
+IFS=$SAVEIFS
+dns-sd -R "$i""2" _ipp._tcp,_universal . 631 $Options & sleep 1 &
 done
 
 
 
-IFS=$SAVEIFS
+
 
 
 
